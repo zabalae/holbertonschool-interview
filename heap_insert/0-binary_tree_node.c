05535717255f9d0bc13db001ecf008ce01e7065c
Elizabeth Mendez <40727857+zabalae@users.noreply.github.com>
2024-09-15 15:09:33 -0400

    task 0

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -10,18 +10,18 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+binary_tree_t *new_node;
 
-    /* Allocate memory for the new node */
-    new_node = malloc(sizeof(binary_tree_t));
-    if (!new_node)
-        return (NULL);
+/* Allocate memory for the new node */
+new_node = malloc(sizeof(binary_tree_t));
+if (!new_node)
+    return (NULL);
 
-    /* Initialize the new node */
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
+/* Initialize the new node */
+new_node->n = value;
+new_node->parent = parent;
+new_node->left = NULL;
+new_node->right = NULL;
 
-    return (new_node);
+return (new_node);
 }
