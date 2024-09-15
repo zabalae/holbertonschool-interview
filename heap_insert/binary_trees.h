5f57ef893d10dd7f83c7e427e29a46a7c3b0b4a9
Elizabeth Mendez <40727857+zabalae@users.noreply.github.com>
2024-09-15 15:35:32 -0400

    task 1

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -18,7 +18,9 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s heap_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+heap_t *heap_insert(heap_t **root, int value);
 
 #endif /* BINARY_TREES_H */
