449ee9d3d27c05adaf47513c3440c80ca96f0edb
Elizabeth Mendez <40727857+zabalae@users.noreply.github.com>
2024-11-23 22:27:02 -0400

    task 0

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -2,42 +2,84 @@
 '''
 Module that solves the N queens problem
 '''
-from sys import argv, exit
+import sys
+
+
+def board_set_up(N):
+    """
+    Sets up blank NxN chessboard
+    """
+    matrix = []
+    for row in range(N):
+        matrix_row = []
+        for column in range(N):
+            matrix_row.append(0)
+        matrix.append(matrix_row)
+    return (matrix)
+
+
+def print_solution(matrix):
+    """
+    Prints the coordinates where there is a queen
+    """
+    queens_coordinates = []
+    for i, row in enumerate(matrix):
+        for j, column in enumerate(row):
+            if column == 1:
+                queen = []
+                queen.append(i)
+                queen.append(j)
+                queens_coordinates.append(queen)
+    print(queens_coordinates)
+
+
+def is_safe(matrix, new_row, new_column):
+    """
+    Determines if a queen is safe to be put in new_row, new_column
+    """
+    for i in range(new_column):
+        if matrix[new_row][i]:
+            return False
+    for i, j in zip(range(new_row, -1, -1),
+                    range(new_column, -1, -1)):
+        if matrix[i][j]:
+            return False
+    N = len(matrix)
+    for i, j in zip(range(new_row, N, 1),
+                    range(new_column, -1, -1)):
+        if matrix[i][j]:
+            return False
+    return True
+
+
+def solve(matrix, new_column):
+    """
+    Recursively solves the N Queens puzzle
+    """
+    N = len(matrix)
+    if new_column >= N:
+        print_solution(matrix)
+        return matrix
+    for new_row in range(N):
+        if is_safe(matrix, new_row, new_column):
+            matrix[new_row][new_column] = 1
+            solve(matrix, new_column + 1)
+            matrix[new_row][new_column] = 0
+    return None
+
 
 if __name__ == "__main__":
-    if len(argv) != 2:
-        print('Usage: nqueens N')
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
         exit(1)
+    N = sys.argv[1]
     try:
-        n = int(argv[1])
-    except BaseException:
-        print('N must be a number')
+        N = int(N)
+    except Exception as e:
+        print("N must be a number")
         exit(1)
-    if n < 4:
-        print('N must be at least 4')
+    if N < 4:
+        print("N must be at least 4")
         exit(1)
-
-    solution = []
-
-    def solve_queens(row, n, solution):
-        if (row == n):
-            print(solution)
-        else:
-            for col in range(n):
-                placement = [row, col]
-                if valid_placement(solution, placement):
-                    solution.append(placement)
-                    solve_queens(row + 1, n, solution)
-                    solution.remove(placement)
-
-    def valid_placement(solution, placement):
-        for queen in solution:
-            if queen[1] == placement[1]:
-                return False
-            if (queen[0] + queen[1]) == (placement[0] + placement[1]):
-                return False
-            if (queen[0] - queen[1]) == (placement[0] - placement[1]):
-                return False
-        return True
-
-    solve_queens(0, n, solution)
+    matrix = board_set_up(N)
+    solve(matrix, 0)
