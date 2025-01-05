200c8ac20536e711991b3d3e0bbc5d770ca5b264
Elizabeth Mendez <40727857+zabalae@users.noreply.github.com>
2025-01-05 19:10:06 -0400

    task

---
+++ advanced_binary_search/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef _ADVANCESEARCH_H_
+#define _ADVANCESEARCH_H_
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int advanced_binary(int *array, size_t size, int value);
+
+#endif /* _ADVANCESEARCH_H_ */
